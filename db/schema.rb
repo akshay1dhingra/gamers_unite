af7b8d09691b76a6cd1bfe911d4175cae5f2f1ba
Akshay Dhingra <akshay1dhingra@gmail.com>
2019-01-16 16:15:16 -0600

    added username field to schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2019_01_16_212609) do
 
   create_table "users", force: :cascade do |t|
+    t.string "username"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
