61935b5457437a374452d12f0618cb3fcb3727e3
Akshay Dhingra <akshay1dhingra@gmail.com>
2019-01-16 17:10:02 -0600

    updated routes file

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'test#home'
+
+  resources :games 
+  resources :reviews
+  
 end
