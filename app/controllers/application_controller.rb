af438fd66b1055a894f3a518719da27135fa0d84
Akshay Dhingra <akshay1dhingra@gmail.com>
2019-01-17 15:34:57 -0600

    added methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
 
+    def home_page
+      if user_signed_in?
+        redirect_to games_path
+      end
+    end
+
+    def go_log_in 
+      flash[:alert] = "Please log in!"
+      redirect_to root_path
+    end 
+
     protected
 
     def configure_permitted_parameters
