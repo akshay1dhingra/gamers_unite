c6a80fd0ea1d1b5b40999d27eca6bbc859498632
Akshay Dhingra <akshay1dhingra@gmail.com>
2019-01-17 14:08:49 -0600

    updated route path again

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     sessions: 'users/sessions'
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'games#index'
+  root 'application#home_page'
   
   resources :reviews
 
