3cf32241648790101b07637491420f9734896fd6
Akshay Dhingra <akshay1dhingra@gmail.com>
2019-01-17 14:08:02 -0600

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
     sessions: 'users/sessions'
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'test#home'
-
-  resources :games 
-  resources :reviews
+  root 'games#index'
   
+  resources :reviews
+
+  resources :games do 
+    resources :reviews 
+  end
+
 end
