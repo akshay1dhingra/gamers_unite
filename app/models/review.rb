accb4222de9b4b2f07ca06e0636de1cc4bef96b8
Akshay Dhingra <akshay1dhingra@gmail.com>
2019-01-28 11:24:29 -0600

    updated review model with validators and scope

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,10 @@
 class Review < ApplicationRecord
     belongs_to :user
     belongs_to :game
+
+    validates :title, presence: true
+    validates :content, presence: true
+    validates :score, presence: true
+
+    scope :score, -> (number) { where ("score < ?"), number }
 end
