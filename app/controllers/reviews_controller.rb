fe69016ddf694e5e1c43160e634b078193b1e9e9
Akshay Dhingra <akshay1dhingra@gmail.com>
2019-01-28 11:24:47 -0600

    updated review#new controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,10 +2,15 @@ class ReviewsController < ApplicationController
     
     def index
         @reviews = Review.all
+        @user = current_user
     end
 
     def new
-        @review = Review.new
+        if user_signed_in?
+            @review = Review.new
+        else 
+            go_log_in
+        end
     end
 
     def create
