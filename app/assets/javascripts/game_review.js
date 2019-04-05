c54dc09534513a5782e3e0790594d45ecc2a52fe
Akshay Dhingra <akshay1dhingra@gmail.com>
2019-04-04 20:53:32 -0500

    add js fnc to display clickable list of games on game/index page

--- app/assets/javascripts/game_review.js
+++ app/assets/javascripts/game_review.js
@@ -3,5 +3,16 @@ $(document).ready(function () {
 })
 
 function attachListeners() {
+    $('#see_more_games_button').click(getGames)
+}
 
+function getGames() {
+    // event.preventDefault()
+    $(this).hide();
+    $.get(`/games.json`, function(data){
+        console.log(data)
+        data.map(game => {
+            $('#games').append($('<a href="games/'+ game.id +'"><br>'+ game.name +'<br>'))
+        })
+    })
 }
