83a20e95889f3c8adcc63d4f2f6d01d64529e270
Akshay Dhingra <akshay1dhingra@gmail.com>
2019-04-07 17:30:53 -0500

    add js to show reviews on game show page

--- app/assets/javascripts/game_review.js
+++ app/assets/javascripts/game_review.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
 
 function attachListeners() {
     $('#see_more_games_button').click(getGames)
+    $('#show_review_link').click(showReviews)
 }
 
 function getGames() {
@@ -13,6 +14,21 @@ function getGames() {
         console.log(data)
         data.map(game => {
             $('#games').append($('<a href="games/'+ game.id +'"><br>'+ game.name +'<br>'))
+        })
+    })
+}
+
+function showReviews() {
+    event.preventDefault()
+    $(this).hide();
+    let id = parseInt($("#show_review_link").attr("data-id"))
+    $.get(`/games/${id}.json`, function(data){
+        data.reviews.map(r => {
+            let review = `<h3><a href="/games/${id}/reviews/${r.id}">${r.title}</a></h3><h4>${r.score}/10</h4><p>content: ${r.content}</p>`
+            $.get(`/games/${id}/reviews/${r.id}.json`, function(data){
+                let username = data.user.username
+                $('#review_list_div').append(review + 'written by: ' + username)
+            })
         })
     })
 }
